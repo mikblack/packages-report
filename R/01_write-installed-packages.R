8dbe0247815a741cbb400371ec1d1071d11e68f1
mikblack <blackmail@gmail.com>
2018-02-02 08:40:43 +1300

    First coding task

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,5 +1,10 @@
 ## deja vu from yesterday!
 
+## Load packages
+library(fs)
+library(tidyverse)
+library(here)
+
 ## create a data frame of your installed packages
 
 ## keep the variables
@@ -9,10 +14,16 @@
 ##   * Priority
 ##   * Built
 
+ipt <- installed.packages() %>%
+  as_data_frame() %>%
+  select(., c("Package", "LibPath", "Version", "Priority","Built"))
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+write_csv(ipt, here("data/installed-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
